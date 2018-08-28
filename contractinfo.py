ead05864b67e058865119f81200fdfb233526d43
Darren Ross <darren0170@gmail.com>
2018-08-28 07:28:19 -0400

    Sped it up

--- contractinfo.py
+++ contractinfo.py
@@ -16,30 +16,37 @@ w3 = Web3(HTTPProvider(apihost))
 if not w3.isAddress(args.contractAddress):
     sys.exit('\n' + args.contractAddress + ' is not a valid eth address\n')
 
-
 contractAddress = w3.toChecksumAddress(args.contractAddress)
+upper = w3.eth.blockNumber
+lower = 0
+searching = True
 
-blockNumber = w3.eth.blockNumber
-# use below for a quick test
-# blockNumber = 3978300
-
-code = w3.eth.getCode(contractAddress, block_identifier=blockNumber)
-
-if code:
-    while code:
-        code = w3.eth.getCode(contractAddress, block_identifier=blockNumber)
-        blockNumber -= 1
-    contractBlockNumber = blockNumber + 2
-    contractBlock = w3.eth.getBlock(contractBlockNumber)
-    contractBlockHash = contractBlock['hash'].hex()
-    for tx in contractBlock['transactions']:
-        txReceipt = w3.eth.getTransactionReceipt(tx)
-        if txReceipt['contractAddress'] == contractAddress:
-            contractTx = tx.hex()
-            break
-
-    print( '\nBlock: ' + str(contractBlockHash))
-    print( 'Transaction: ' + str(contractTx) + '\n')
-else:
+code = w3.eth.getCode(contractAddress, block_identifier=upper)
+if not code:
     sys.exit('\nErr: it appears that ' + contractAddress + ' is not a contract address\n')
+
+while searching:
+    midling = (lower + upper) // 2
+
+    if lower == midling:
+        searching = False
+        contractBlockNumber = upper
+        contractBlock = w3.eth.getBlock(contractBlockNumber)
+        contractBlockHash = contractBlock['hash'].hex()
+        for tx in contractBlock['transactions']:
+            txReceipt = w3.eth.getTransactionReceipt(tx)
+            if txReceipt['contractAddress'] == contractAddress:
+                contractTx = tx.hex()
+
+        print( '\nBlock: ' + str(contractBlockHash))
+        print( 'Transaction: ' + str(contractTx) + '\n')
+
+        break
+
+    code = w3.eth.getCode(contractAddress, block_identifier=midling)
+
+    if code:
+        upper = midling
+    elif not code :
+        lower = midling
 
